41adf1aeae735ef0ca2a4c6322f1822552fa808b
abdullZiba <87323265+abdullZiba@users.noreply.github.com>
2021-07-13 23:56:46 +0300

    Update googleBot.js

--- googleBot.js
+++ googleBot.js
@@ -4,49 +4,53 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.google.com/*
-// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://yandex.ru/*
+// @match        https://музыкалка-онлайн.рф/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-let googleInput = document.getElementsByName("q")[0];
-let btnK = document.getElementsByName("btnK")[1];
-
-if(btnK != undefined){ // Проверяем, что мы на главной странице
-    googleInput.value = "гобой"; // Пишем фразу в поисковую строку
+let yandexInput = document.getElementById("text");
+let btnK = document.getElementsByClassName("button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited")[0];
+if(btnK != undefined){
+    yandexInput.value = "Что такое гобой";
     setTimeout(function(){
-        btnK.click();// Клик по кнопке поиска
+        btnK.click();
     }, 1000);
-}else if(location.hostname === "www.google.com"){ // Если страница с поисковой выдачей
-    let links = document.links; // Собираем коллекцию ссылок
+}else if(location.hostname === "yandex.ru"){
+    let links = document.links;
     let goNext = true;
-    for(let i=0; i<links.length; i++){ // Перебираем ссылки
+    for(let i=0; i<links.length; i++){
         let link = links[i];
-        if(link.href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){ // Ищем ссылку с нужным сайтом
+        if(link.href.indexOf("музыкалка-онлайн.рф") != -1){
             setTimeout(function(){
-                link.click(); // Кликаем по ссылке с нужным сайтом
+                link.click();
             }, 3000);
-            goNext = false; // запрещаем идти дальше по страницам поисковика
-            break; // Останавливаем цикл
+            goNext = false;
+            break;
         }
     }
-    if(goNext){ // Проверяем, можно ли идти далее по страницам поисковика
-        let pnnext = document.getElementById("pnnext"); // Находим кнопку "Следующая"
+
+
+
+
+
+    if(goNext){
+        let pnnext = document.getElementsByClassName("link link_theme_none link_target_serp pager__item pager__item_kind_next i-bem link_js_inited")[0];
         setTimeout(function(){
-            pnnext.click(); // Кликаем по кнопке следующая
+            pnnext.click();
         }, 3000);
     }
-}else{ // Любой другой сайт
-    let links = document.links; // Коллекция ссылок
+}else{
+    let links = document.links;
     let randomIndex = getIntRandom(0, links.length);
     let link = links[randomIndex];
-    if(link.href.indexOf(location.hostname) != -1){ // Если переход внутри сайта
+    if(link.href.indexOf(location.hostname) != -1){
         setTimeout(function(){
             links[randomIndex].click();
         }, 2000);
-    }else{ // Если переход на другой сайт, то мы ссылаем браузер на главную страницу нашего сайта
-        location.href = "https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/";
+    }else{
+        location.href = "https://музыкалка-онлайн.рф/";
     }
 }
 
